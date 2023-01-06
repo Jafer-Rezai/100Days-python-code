61cadff1603705fd6fc323c0408c55f446386e43
Jafer-Rezai <drjafer.rezai@gmail.com>
2023-01-06 17:38:58 +0100

    Hello pythoh

---
+++ Day1.py
@@ -0,0 +1,5 @@
+# Day one Welcome to start our first day code
+print("Welcome to the Band Name Generator.")
+print(input("What is name of the city you grew up in?: "))
+print(input("What is your pet's name?: "))
+print()
